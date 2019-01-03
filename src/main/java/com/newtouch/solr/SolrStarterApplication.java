51e2fcd15cc7975b6437a225632f0e65223bb5cb
DragonWang <185138868@qq.com>
2019-01-03 14:45:19 +0800

    提交solr application spring boot 项目

---
+++ src/main/java/com/newtouch/solr/SolrStarterApplication.java
@@ -0,0 +1,15 @@
+package com.newtouch.solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dragon on 2019/1/3.
+ */
+@SpringBootApplication
+public class SolrStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolrStarterApplication.class, args);
+    }
+}
